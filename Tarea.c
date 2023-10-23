2b00b2ac3332c2475e0364f58ad5147ca268de93
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2023-10-22 22:42:26 -0500

    2. Generando la estructura e inicializando la tabla hash

--- Tarea.c
+++ Tarea.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct HashTable {
+    int n;
+    int (*h)(int, int);
+    int **bucket;
+};
+
+struct HashTable* initHashTable(int (*hashFn)(int, int), int n) {
+    struct HashTable* ht = (struct HashTable*)malloc(sizeof(struct HashTable));
+    ht->n = n;
+    ht->h = hashFn;
+    ht->bucket = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        ht->bucket[i] = NULL;
+    }
+    return ht;
+}
 
 int main()
 {
